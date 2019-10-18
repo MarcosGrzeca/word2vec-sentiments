eca900bdbe80570a05f933850ff63087ede83963
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-10-17 21:53:25 -0300

    Update word2vec-sentiments_modified.py

--- word2vec-sentiments_modified.py
+++ word2vec-sentiments_modified.py
@@ -60,15 +60,18 @@ class TaggedLineSentence(object):
 
 log.info('source load')
 train_source = {'train-neg.txt':'TRAIN_NEG', 'train-pos.txt':'TRAIN_POS', 'test-neg.txt':'TEST_NEG', 'test-pos.txt':'TEST_POS', 'train-unsup.txt':'TRAIN_UNS'}
+# train_source = {'train-neg.txt':'TRAIN_NEG', 'train-pos.txt':'TRAIN_POS', 'test-neg.txt':'TEST_NEG', 'test-pos.txt':'TEST_POS'}
 test_source = {'test-neg.txt':'TEST_NEG', 'test-pos.txt':'TEST_POS'}
 
 log.info('TaggedDocument')
 train_sentences = TaggedLineSentence(train_source)
 test_sentences = TaggedLineSentence(test_source)
 
+marcos = train_sentences.to_array()
+
 
 log.info('D2V')
-model = Doc2Vec(min_count=1, window=10, size=150, sample=1e-4, negative=5, workers=7,iter=50)
+model = Doc2Vec(min_count=1, window=10, vector_size=150, sample=1e-4, negative=5, workers=7,iter=40)
 model.build_vocab(train_sentences.to_array())
 
 log.info('Epoch')
@@ -77,8 +80,8 @@ log.info('Epoch')
 model.train(train_sentences.sentences_perm(),total_examples=model.corpus_count,epochs=model.iter)
 
 log.info('Model Save')
-model.save('./imdb.d2v')
-model = Doc2Vec.load('./imdb.d2v')
+model.save('./imdbc.d2v')
+model = Doc2Vec.load('./imdbc.d2v')
 
 log.info('Sentiment')
 train_arrays = numpy.zeros((25000, 150))
@@ -101,21 +104,29 @@ log.info(train_labels)
 test_arrays = numpy.zeros((25000, 150))
 test_labels = numpy.zeros(25000)
 
+i = 0
+# for index, i in enumerate(test_sentences):
+for i in range(12500):
+    prefix_test_pos = 'TEST_POS_' + str(i)
+    prefix_test_neg = 'TEST_NEG_' + str(i)
+    # feature = model.infer_vector(i[0])
 
-for index, i in enumerate(test_sentences):
-    # prefix_test_pos = 'TEST_POS_' + str(i)
-    # prefix_test_neg = 'TEST_NEG_' + str(i)
-    feature = model.infer_vector(i[0])
-    if index <12500:
-        test_arrays[index] = feature
-        test_labels[index] = 0
-    else:
-        test_arrays[index] = feature
-        test_labels[index] = 1
+    # if index <12500:
+    test_arrays[i] = model.docvecs[prefix_test_pos]
+    test_arrays[12500 + i] = model.docvecs[prefix_test_neg]
+    test_labels[i] = 1
+    test_labels[12500 + i] = 0
+
 
 log.info('Fitting')
 classifier = LinearSVC()
 classifier.fit(train_arrays, train_labels)
+
+#numpy.savetxt("kagggle_train_LogisticRegression_labels.csv", numpy.asarray(train_labels), delimiter=",")
+#numpy.savetxt("kagggle_teste_LogisticRegression_labels.csv", numpy.asarray(test_labels), delimiter=",")
+
+# classifier = LogisticRegression()
+# classifier.fit(train_arrays, train_labels)
 
 # LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
 #           intercept_scaling=1, penalty='l2', random_state=None, tol=0.0001)
